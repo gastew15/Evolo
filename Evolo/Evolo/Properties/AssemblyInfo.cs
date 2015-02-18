1ad6d06601c88ba65d7c8ee151996f467079437a
G. Stewart <gastewserver@hotmail.com>
2015-02-17 20:25:30 -0500

    Chages for signing and creating installers

--- Evolo/Evolo/Properties/AssemblyInfo.cs
+++ Evolo/Evolo/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Evolo")]
 [assembly: AssemblyProduct("Evolo")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Ingham ISD")]
+[assembly: AssemblyDescription("Tetris Platforming Game")]
+[assembly: AssemblyCompany("Twisted Transistors Studios")]
 [assembly: AssemblyCopyright("Copyright © Ingham ISD 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,3 +33,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
