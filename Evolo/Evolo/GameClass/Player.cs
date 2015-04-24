12f3d175a5deb39821060d5190e1297311b69cda
G. Stewart <gastewserver@hotmail.com>
2015-04-24 08:03:25 -0400

    Modified player class to be able to hopefully handle free-form movement. The field Manager implementation is not finished at all, and was just modified to get rid of compile errors temporarily.

--- Evolo/Evolo/GameClass/Player.cs
+++ Evolo/Evolo/GameClass/Player.cs
@@ -1,5 +1,4 @@
-﻿#region Using
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,37 +6,122 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
-#endregion
 
 /** Class Discription
  * Evolo class to create and handle player objects in the game
  * Author: Gavin
- * Version: 10/21/14
+ * Version: 4/24/14
  */
 
 namespace Evolo.GameClass
 {
     class Player
     {
-        Vector2 drawPoint, scaleSize;
+        Vector2 position, scaleSize;
         Texture2D playerTexture;
         Color drawColor;
+        double xVelocity = 0.0;
+        double yVelocity = 0.0;
+        double gravity;
+        double friction;
 
-        public Player(Texture2D playerTexture)
+
+        public Player(Texture2D playerTexture, Vector2 position, double gravity, double friction)
         {
             this.playerTexture = playerTexture;
+            this.position = position;
+            this.gravity = gravity;
+            this.friction = friction;
         }
 
-        public void Update(Vector2 drawPoint, Vector2 scaleSize, Color drawColor)
+        public void Update(Vector2 scaleSize)
         {
-            this.drawPoint = drawPoint;
             this.scaleSize = scaleSize;
-            this.drawColor = drawColor;
+
+            position.X += (float)xVelocity * scaleSize.X;
+            position.Y += (float)yVelocity * scaleSize.Y;
+
+            if (xVelocity > 0 && xVelocity - friction >= 0)
+                xVelocity -= friction * scaleSize.X;
+            else if(xVelocity < 0 && xVelocity + friction >= 0)
+                xVelocity += friction * scaleSize.X;
+            else
+                xVelocity = 0;
+
+            if (yVelocity - gravity >= 0)
+                yVelocity -= gravity * scaleSize.Y;
+            else
+                yVelocity = 0;
         }
 
         public void Draw(SpriteBatch spriteBatch, SpriteEffects spriteEffects)
         {
-            spriteBatch.Draw(playerTexture, drawPoint, null, drawColor, 0f, new Vector2(0, 0), scaleSize, spriteEffects, 1f); 
+            spriteBatch.Draw(playerTexture, position, null, drawColor, 0f, new Vector2(0, 0), scaleSize, spriteEffects, 1f); 
+        }
+
+        public void Jump()
+        {
+            yVelocity = 2.0;
+        }
+
+        public Color getColor()
+        {
+            return drawColor;
+        }
+
+        public void setColor(Color color)
+        {
+            drawColor = color;
+        }
+
+        public void setPosition(Vector2 position)
+        {
+            this.position = position;
+        }
+
+        public Vector2 getPosition()
+        {
+            return position;
+        }
+
+        public double getGravity()
+        {
+            return gravity;
+        }
+
+        public void setGravity(double gravity)
+        {
+            this.gravity = gravity;
+        }
+
+        public double getFriction()
+        {
+            return friction;
+        }
+
+        public void setFriction(double friction)
+        {
+            this.friction = friction;
+        }
+
+        public double getXVelocity()
+        {
+            return xVelocity;
+        }
+
+        public void setXVelocity(double xVelocity)
+        {
+
+        }
+
+        public double getYVelocity()
+        {
+            return yVelocity;
+        }
+
+        public void setYVelocity(double yVelocity)
+        {
+
         }
     }
 }
